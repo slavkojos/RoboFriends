ad11b2d8f33d5d2e5c841547bede3e3b721f5f7c
slavko <slavkoj6@gmail.com>
2020-06-04 04:11:10 +0200

    minor tweaks

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({id, name , email }) => {
     
     return (
         <Fragment>
-        <div className="tc dib br3 ma2 grow bw2 shadow-5" style ={{backgroundColor:"#90e0ef"}}>
+        <div className="tc dib br4 ma3 grow bw2 shadow-5" style ={{backgroundColor:"#90e0ef"}}>
             <img src={`https://robohash.org/${id}?200x200`} alt="robohash"/>
             <div>
                 <h2>{name}</h2>
